e778787ca1fa765418fb30e1bc7ac1601f9e61df
Sam Rawlins <sam.rawlins@gmail.com>
2013-06-18 08:11:12 -0700

    Um... so much has changed

--- sass-it.sh
+++ sass-it.sh
@@ -1,1 +1,5 @@
+echo sass --scss assets/stylesheets/application.css.scss assets/stylesheets/application.css
 sass --scss assets/stylesheets/application.css.scss assets/stylesheets/application.css
+
+echo sass --scss assets/stylesheets/google-form.css.scss assets/stylesheets/google-form.css
+sass --scss assets/stylesheets/google-form.css.scss assets/stylesheets/google-form.css
